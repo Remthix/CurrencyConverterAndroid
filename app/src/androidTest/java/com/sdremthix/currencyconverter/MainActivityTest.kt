98713a5a482c91b8636bd4ca52949afc70ff652b
Srki <sdremthix@gmail.com>
2020-09-13 14:07:13 +0200

    Added testing dependencies. Making first test pass and fail. Changed version name to: 1.0.0

---
+++ app/src/androidTest/java/com/sdremthix/currencyconverter/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.sdremthix.currencyconverter
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * The Main view component testing suite.
+ */
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @get:Rule
+    val mMainActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun defaultProjectSetupTextIsShowing_pass() {
+        onView(withText("Hello World!")).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun defaultProjectSetupTextIsShowing_fail() {
+        onView(withText("Hello world!")).check(doesNotExist())
+    }
+}
